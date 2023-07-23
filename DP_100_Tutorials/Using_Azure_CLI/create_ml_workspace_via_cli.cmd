ddda82b002705366bc4c59ddd319d806ee1ce370
aidowu1 <adeidowu@hotmail.com>
2023-07-23 10:18:53 +0100

    Modified Azure ML Workspace setup

---
+++ DP_100_Tutorials/Using_Azure_CLI/create_ml_workspace_via_cli.cmd
@@ -0,0 +1,12 @@
+# List resource groups
+az group list -o table
+
+#Create a new resource group
+az group create --name packt-azureml-rg -l uksouth
+
+# Create a new Azure ml workspace
+az extension add -n ml
+az ml workspace create -n ml-demo3-ws -g packt-azureml-rg
+
+## Delete the workspace
+az ml workspace delete -n ml-demo3-ws -g packt-azureml-rg 
