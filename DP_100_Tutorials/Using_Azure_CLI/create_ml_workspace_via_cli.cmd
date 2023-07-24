3d4794d6591c9e153d671f1ed11cccb172b6b01d
aidowu1 <adeidowu@hotmail.com>
2023-07-24 15:32:39 +0100

    Update create_ml_workspace_via_cli.cmd

--- DP_100_Tutorials/Using_Azure_CLI/create_ml_workspace_via_cli.cmd
+++ DP_100_Tutorials/Using_Azure_CLI/create_ml_workspace_via_cli.cmd
@@ -8,5 +8,8 @@ az group create --name packt-azureml-rg -l uksouth
 az extension add -n ml
 az ml workspace create -n ml-demo3-ws -g packt-azureml-rg
 
+# Delete Resource group
+az group delete --name packt-azureml-rg
+
 ## Delete the workspace
 az ml workspace delete -n ml-demo3-ws -g packt-azureml-rg 
